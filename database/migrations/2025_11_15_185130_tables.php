3fc979f01dce7298bc330deffcac39e97624e948
enriss04 <enriss-01@hotmail.com>
2026-01-20 22:32:56 -0600

    agregando tabla

--- database/migrations/2025_11_15_185130_tables.php
+++ database/migrations/2025_11_15_185130_tables.php
@@ -116,7 +116,23 @@ return new class extends Migration
             $table->primary('venta_id');
         });
 
-       
+        Schema::create('ventas_mercancias', function (Blueprint $table) {
+            $table->id('venta_mercancia_id'); 
+            $table->string('venta_id', 50)->nullable();            
+            $table->string('inventario_id', 20)->nullable();
+            $table->enum('tipo', ['Mayoreo', 'Menudeo'])->default('Menudeo');
+            $table->integer('cantidad')->default(0);
+            $table->double('precio_unitario', 15, 2)->default(0);
+            $table->double('descuento', 15, 2)->default(0);
+            $table->double('subtotal', 15, 2)->default(0);
+            $table->double('total', 15, 2)->default(0);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'))->useCurrentOnUpdate(); 
+            $table->softDeletes();
+            $table->index('venta_id');
+            $table->index('inventario_id');
+            $table->primary('venta_mercancia_id');
+        });
     }
 
     /**
