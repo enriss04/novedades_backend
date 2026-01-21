aad47f97fdc397d5e86ddb9631203947e479ae21
enriss04 <enriss-01@hotmail.com>
2026-01-20 22:47:34 -0600

    agregan tablas

--- database/migrations/2025_11_15_185130_tables.php
+++ database/migrations/2025_11_15_185130_tables.php
@@ -115,24 +115,6 @@ return new class extends Migration
             $table->softDeletes();
             $table->primary('venta_id');
         });
-
-        Schema::create('ventas_mercancias', function (Blueprint $table) {
-            $table->id('venta_mercancia_id'); 
-            $table->string('venta_id', 50)->nullable();            
-            $table->string('inventario_id', 20)->nullable();
-            $table->enum('tipo', ['Mayoreo', 'Menudeo'])->default('Menudeo');
-            $table->integer('cantidad')->default(0);
-            $table->double('precio_unitario', 15, 2)->default(0);
-            $table->double('descuento', 15, 2)->default(0);
-            $table->double('subtotal', 15, 2)->default(0);
-            $table->double('total', 15, 2)->default(0);
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'))->useCurrentOnUpdate(); 
-            $table->softDeletes();
-            $table->index('venta_id');
-            $table->index('inventario_id');
-            $table->primary('venta_mercancia_id');
-        });
     }
 
     /**
@@ -147,6 +129,5 @@ return new class extends Migration
         Schema::dropIfExists('pedidos');
         Schema::dropIfExists('pedidos_mercancias');
         Schema::dropIfExists('ventas');
-        Schema::dropIfExists('ventas_mercancias');
     }
 };
